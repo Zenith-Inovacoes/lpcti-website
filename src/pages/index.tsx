8d1ab00b3510774a17b81334e1ec86400beb85e1
Rian Ernesto <rianernesto9@gmail.com>
2023-01-20 11:49:39 -0300

    :sparkles: feat: Componente de botão adicionado com duas variantes, uma de rounded, que trata o botão como arredondado e espera apenas um ícone dentro dele, e outro text, que espera um texto ou texto e ícone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,10 @@ import MainSection from '../components/Home/mainSection/mainSection'
 import SectionTitle from '@/components/SectionTitle/SectionTitle'
 import Typography from '@/components/Typography/Typography'
 import Card from '@/components/Card/Card'
+import Button from '@/components/Button'
+
 import Image from 'next/image'
+import InstagramIcon from '@/assets/icons/instagram-fill.svg'
 
 export default function Home() {
   return (
@@ -12,7 +15,6 @@ export default function Home() {
       <Head>
         <title>LPCTI</title>
       </Head>
-      <MainSection />
     </>
   )
 }
