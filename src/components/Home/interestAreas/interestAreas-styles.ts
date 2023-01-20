50a5c1e3cacbd0d2c4ddfdca01c7cc64e581efd9
Tavares <lucas_carvalhok@outlook.com>
2023-01-20 18:41:47 -0300

    :recycle: refactor: Ajustes de qualidade de imagem e centralização das divs

--- src/components/Home/interestAreas/interestAreas-styles.ts
+++ src/components/Home/interestAreas/interestAreas-styles.ts
@@ -4,20 +4,40 @@ import * as S from '@/components/Typography/styles'
 export const background = styled.div`
   width: 100%;
   height: auto;
-  padding-top: 11rem;
+  padding-top: 9rem;
+  padding-bottom: 1rem;
 `;
 
 export const divText = styled.div`
   align-content: center;
   text-align: center;
   min-width: 21.875rem;
-  margin: 2.5rem;
-  margin-bottom: 0;
+  padding: 2.5rem 4.5rem;
+  padding-bottom: 0;
   > ${S.TypographyRoot} {
     display: inline-block;
     margin: 20px 0;
 }
 `;
+
+export const cardText = styled.div`
+margin-right: 3.3rem;
+margin-left: 3.3rem;
+  > ${S.TypographyRoot} {
+    display: inline-block;
+    margin: 20px 0;
+    }
+`
+export const iotImg = styled.div`
+width: 100%;
+position: absolute;
+display: flex;
+justify-content: space-between;
+> img:not(:first-child) {
+  transform: skewY(-35deg);
+  transform: translateY(33%);
+}
+`
 
 export const cardPosition = styled.div`
 display: flex;
--- src/components/Home/interestAreas/interestAreas.tsx
+++ src/components/Home/interestAreas/interestAreas.tsx
@@ -4,17 +4,23 @@ import Card from '@/components/Card/Card';
 import cicleCard from '@/assets/images/tech-circle.svg'
 import userIcon from '@/assets/icons/user-icon.svg'
 import userBackground from '@/assets/images/userBackground.svg'
+import iot from '@/assets/images/IOT LINES 1.png'
+import iot2 from '@/assets/images/IOT LINES 2.png'
 import Image from 'next/image';
 
 export default function InterestAreas() {
 
     return (
         <S.background>
+            <S.iotImg>
+                <Image alt='iot lines' src={iot2}></Image>
+                <Image alt='iot lines' src={iot}></Image>
+            </S.iotImg>
             <S.divText>
                 <Typography variant='h4' color='#29156D'>
                     Áreas de <Typography variant='h4' color='#00C0F0' display='inline'> interesse</Typography>
                 </Typography>
-                <Typography variant='h6' weight='100' color='#29156D'>
+                <Typography variant='h6' weight='210' color='#29156D'>
                     Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
                     consequat duis enim velit mollit. Exercitation veniam ...
                 </Typography>
@@ -26,10 +32,12 @@ export default function InterestAreas() {
                         <Image alt='background' src={userBackground} width={130}></Image>
                         <Image alt='user icon' src={userIcon}width={70}></Image>
                     </S.divImage>
-                    <Typography variant='h6' weight='100' color='#29156D'>
-                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                        consequat duis enim velit mollit. Exercitation veniam ...
+                    <S.cardText>
+                    <Typography variant='h3' color='#29156D'>Lorem Ipsum</Typography>
+                    <Typography variant='h6' weight='250' color='#29156D'>
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia...
                     </Typography>
+                    </S.cardText>
                 </Card.Root>
             </S.cardPosition>
         </S.background>
