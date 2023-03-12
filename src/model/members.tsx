6f3ff477ee92466b56ec3bfcf4cdedc61af7d903
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-03-11 22:10:42 -0300

    :sparkles: feat(membersSection): detalhes do membro

--- src/model/members.tsx
+++ src/model/members.tsx
@@ -4,45 +4,55 @@ import Lucas from '@/assets/images/Foto Lucas Gabriel.jpg'
 export const members = [
     {
         Name: "Gabriel  Marcelino",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/ 
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/ 
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/" /* link do linkedin do membro */
     },
     {
         Name: "Caique Mauricio",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/ 
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/ 
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/" /* link do linkedin do membro */
     },
     {
         Name: "João Renan",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/  
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/  
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/"  /* link do linkedin do membro */
     },
     {
         Name: "João Victor",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/"  /* link do linkedin do membro */
     },
     {
         Name: "Matheus Tsuchiya",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/ 
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/ 
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/"  /* link do linkedin do membro */
     },
     {
         Name: "Rian Ernesto",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/"  /* link do linkedin do membro */
     },
     {
         Name: "Renan, Andrew",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/"  /* link do linkedin do membro */
     },
 ]
 
 export const directors = [
     {
         Name: "Fábio Araújo",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/ 
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/ 
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/"  /* link do linkedin do membro */
     },
     {
         Name: "Sergio Andrade",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/ 
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/ 
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/"  /* link do linkedin do membro */
     },
     {
         Name: "Suzane Alfaia",
-        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />/**foto do membro*/  
+        MemberPicture: () => <Image src={Lucas} alt='Foto de Membro do Laboratorio' />,/**foto do membro*/  
+        Linkedin: "https://www.linkedin.com/in/gabriel-marcelino-200789233/"  /* link do linkedin do membro */
     },
 ]
